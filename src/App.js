35e66ab7a44e1ca9f87dbd931bc3158328047b4f
Yeshwanth <v.yeshwanth2004@gmail.com>
2023-08-27 20:34:55 +0530

    added Preview

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import Hero from './componenets/Hero';
 import Nav from './componenets/Nav';
 import Projects from './componenets/Projects';
 import Skills from './componenets/Skills';
-
+import { Helmet } from 'react-helmet';
+import Preview from './assets/preview.png'
 function App() {
   return (
     <>
+    <Helmet>
+    <meta property='og:image' content={Preview}/>
+            </Helmet>
     <Nav/>
     <Hero/>
     <Skills/>
